648021ec3748fa4444fb5f6f5f34dbff8eba27c1
hhjjoa2817 <hhjjoa2817@naver.com>
2023-11-16 09:12:18 +0900

    Signup 기능 구현/Pre_index 생성/admin 생성

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
+
+app_name = 'accounts'
 
 urlpatterns = [
-    
+    path('', views.pre_index, name='pre_index'),
+    path('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
 
 # Create your views here.
+def pre_index(request):
+    return render(request, 'pre_index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:pre_index')
+    else:
+        form = CustomUserCreationForm()
+    context ={
+        'form': form,
+    }
+    return render(request, 'accounts_form.html', context)
